3a7118888500c28d0b1c2912f1100fc9c8a5a0be
liangfeng <846399316@QQ.com>
2023-03-07 17:44:06 +0800

    增加socket示例

---
+++ socket/tcp_server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+// 处理函数
+func process(conn net.Conn) {
+	defer conn.Close() // 关闭连接
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:]) // 读取数据
+		if err != nil {
+			fmt.Println("read from client failed, err:", err)
+			break
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("rcv client data: ", recvStr)
+		conn.Write([]byte(recvStr)) // 发送数据
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:9999")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept() // 建立连接
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		} else {
+			fmt.Println("new client connect success\n")
+		}
+		go process(conn) // 启动一个goroutine处理连接
+	}
+}
---
+++ socket/udp_server/go.mod
@@ -0,0 +1,3 @@
+module udp_server
+
+go 1.18
